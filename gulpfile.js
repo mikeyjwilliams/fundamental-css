b887e618b53833478d3a16227c2cfd7eac6ea4fa
Mikey Williams <manicdesigner@gmail.com>
2020-06-26 21:45:48 -0400

    update package gulpfile modified too

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,16 @@ const bytediff = require('gulp-bytediff');
 const cssnano = require('gulp-cssnano');
 const cssStats = require('cssstats');
 const csscss = require('gulp-csscss');
+const gls = require('gulp-live-server');
 const rename = require('gulp-rename');
 const stylestats = require('gulp-stylestats');
 const sourcemaps = require('gulp-sourcemaps');
 
 const pixrem = require('pixrem')({
-  rootValue: 16,
-  replace: false,
-  atrules: true,
-  unitPrecision: 3,
+	rootValue: 16,
+	replace: false,
+	atrules: true,
+	unitPrecision: 3
 });
 const pxtorem = require('postcss-pxtorem');
 
@@ -31,28 +32,32 @@ const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 const paths = {
-  styles: {
-    tBase: './builds/base/tail-base.css',
-    tComponent: './builds/base/tail-components.css',
-    tUtilities: './builds/base/tailwind-utilities.css',
-    baseScss: './sass/*.scss',
-    baseCss: './styles/sass-build/*.css',
-    buildCss: './builds/build/*.css',
-    renameCss: './builds/build/build.css',
-    map: './',
-  },
-  dest: {
-    sassDest: './builds/build/',
-    buildMap: './',
-    buildDest: './builds/build/',
-    buildSrc: './builds/build/*.css',
-    miniDest: './public/mini/',
-    miniMap: './',
-    exportBuild: './css-build/',
-    compressDest: './compressed-css/',
-    uncompressDest: './uncompressed-css/',
-    optimized: './optimized/',
-  },
+	styles: {
+		tBase: './builds/base/tail-base.css',
+		tComponent: './builds/base/tail-components.css',
+		tUtilities: './builds/base/tailwind-utilities.css',
+		sassBase: './sass/*.scss',
+		buildsBuildbase: './builds/build/*.css',
+		stylesSassbuildBase: './styles/sass-build/*.css',
+
+		publicBuildBuild: './public/build/build.css',
+		map: './'
+	},
+	dest: {
+		buildMap: './',
+		public: './public/',
+		publicBase: './public/*.css',
+		publicMini: './public/mini/',
+		miniMap: './',
+		uncompressedCss: './uncompressed-css/',
+		compressedCss: './compressed-css/',
+		unoptimized: './unoptimized/',
+		optimized: './optimized/',
+		uncompressedOptimized: 'uncompressed-optimized/',
+		compressededOptimized: 'compressed-optimized/',
+
+		stats: './stats/'
+	}
 };
 
 /**
@@ -60,37 +65,37 @@ const paths = {
  * @description reports back a file size of the css data.
  */
 function humanFileSize(size) {
-  let i = Math.floor(Math.log(size) / Math.log(1024));
-  return (
-    (size / Math.pow(1024, i)).toFixed(2) * 1 +
-    '' +
-    ['B', 'kB', 'MB', 'GB', 'TB'][i]
-  );
+	let i = Math.floor(Math.log(size) / Math.log(1024));
+	return (
+		(size / Math.pow(1024, i)).toFixed(2) * 1 +
+		'' +
+		['B', 'kB', 'MB', 'GB', 'TB'][i]
+	);
 }
 
 function formatByteMessage(source, data) {
-  const prettyStartSize = humanFileSize(data.startSize);
-  let message = '';
-  console.log('D ', data);
-  if (data.startSize !== data.endSize) {
-    const change = data.savings > 0 ? 'saved' : 'gained';
-    const prettySavings = humanFileSize(Math.abs(data.savings));
-    let prettyEndSize = humanFileSize(data.endSize);
+	const prettyStartSize = humanFileSize(data.startSize);
+	let message = '';
+	console.log('D ', data);
+	if (data.startSize !== data.endSize) {
+		const change = data.savings > 0 ? 'saved' : 'gained';
+		const prettySavings = humanFileSize(Math.abs(data.savings));
+		let prettyEndSize = humanFileSize(data.endSize);
 
-    if (data.endSize > data.startSize) {
-      prettyEndSize = chalk.yellow(prettyEndSize);
-    }
-    if (data.endSize < data.startSize) {
-      prettyEndSize = chalk.green(prettyEndSize);
-    }
+		if (data.endSize > data.startSize) {
+			prettyEndSize = chalk.yellow(prettyEndSize);
+		}
+		if (data.endSize < data.startSize) {
+			prettyEndSize = chalk.green(prettyEndSize);
+		}
 
-    message = chalk`${change} ${prettySavings} (${prettyStartSize} -> {bold ${prettyEndSize}})`;
-  } else {
-    message = chalk`kept original filesize. ({bold ${prettyStartSize}})`;
-  }
-  return chalk`{cyan ${source.padStart(12, ' ')}}: {bold ${
-    data.fileName
-  }} ${message}`;
+		message = chalk`${change} ${prettySavings} (${prettyStartSize} -> {bold ${prettyEndSize}})`;
+	} else {
+		message = chalk`kept original filesize. ({bold ${prettyStartSize}})`;
+	}
+	return chalk`{cyan ${source.padStart(12, ' ')}}: {bold ${
+		data.fileName
+	}} ${message}`;
 }
 
 /**
@@ -99,12 +104,12 @@ function formatByteMessage(source, data) {
  * @implements gulp-sass
  */
 function sassy() {
-  return gulp
-    .src(paths.styles.baseScss)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write(paths.dest.buildMap))
-    .pipe(gulp.dest(paths.dest.sassDest));
+	return gulp
+		.src(paths.styles.sassBase)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write(paths.dest.buildMap))
+		.pipe(gulp.dest(paths.dest.public));
 }
 
 /**
@@ -113,15 +118,15 @@ function sassy() {
  * @implements gulp-stylestats
  */
 function stats() {
-  return gulp
-    .src(paths.dest.buildSrc)
-    .pipe(
-      stylestats({
-        type: 'json',
-        outfile: true,
-      })
-    )
-    .pipe(gulp.dest('./stats/'));
+	return gulp
+		.src(paths.dest.publicBase)
+		.pipe(
+			stylestats({
+				type: 'json',
+				outfile: true
+			})
+		)
+		.pipe(gulp.dest(paths.dest.stats));
 }
 
 /**
@@ -130,24 +135,24 @@ function stats() {
  * @implements gulp-postcss, gulp-autoprefixer, postcss-pxtorem
  */
 function pixeltorem() {
-  const processors = [
-    autoprefixer(),
-    pxtorem({
-      exclude: /node_modules/i,
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-      mediaQuery: true,
-      minPixelValue: 0,
-      replace: true,
-    }),
-  ];
+	const processors = [
+		pxtorem({
+			exclude: /node_modules/i,
+			rootValue: 16,
+			unitPrecision: 5,
+			propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+			mediaQuery: true,
+			minPixelValue: 0,
+			replace: true
+		}),
+		autoprefixer()
+	];
 
-  return gulp
-    .src(paths.styles.baseCss)
-    .pipe(postcss(processors))
-    .pipe(gulp.dest(paths.dest.buildDest))
-    .pipe(gulp.dest(paths.dest.uncompressDest));
+	return gulp
+		.src(paths.styles.stylesSassbuildBase)
+		.pipe(postcss(processors))
+		.pipe(gulp.dest(paths.dest.public))
+		.pipe(gulp.dest(paths.dest.uncompressedCss));
 }
 
 /**
@@ -155,16 +160,16 @@ function pixeltorem() {
  * @description renames final build extension to mini.css
  * @implements gulp-rename
  */
-function renameMini() {
-  return gulp
-    .src(paths.dest.buildCss)
-    .pipe(
-      rename(function (path) {
-        path.extname = '.mini.css';
-      })
-    )
-    .pipe(gulp.dest(paths.dest.exportbuildDest));
-}
+// function renameMini() {
+//   return gulp
+//     .src(paths.dest.buildsBuildbase)
+//     .pipe(
+//       rename(function (path) {
+//         path.extname = '.mini.css';
+//       })
+//     )
+//     .pipe(gulp.dest(paths.dest.publicMini));
+// }
 
 /**
  * @name compact
@@ -172,161 +177,178 @@ function renameMini() {
  * @implements gulp-sourcemaps, gulp-cssnano
  */
 function compact() {
-  return gulp
-    .src(paths.styles.buildCss)
-    .pipe(sourcemaps.init())
-    .pipe(cssnano())
-    .pipe(sourcemaps.write(paths.dest.miniMap))
-    .pipe(gulp.dest(paths.dest.compressDest))
-    .pipe(gulp.dest(paths.dest.uncompressDest));
+	return gulp
+		.src(paths.styles.buildsBuildbase)
+		.pipe(sourcemaps.init())
+		.pipe(cssnano())
+		.pipe(sourcemaps.write(paths.dest.miniMap))
+		.pipe(gulp.dest(paths.dest.compressedCss));
 }
+
 function check() {
-  return gulp.src(paths.dest.buildDest + '/').pipe(csscss());
+	return gulp.src(paths.dest.public).pipe(csscss());
 }
 
 function production() {
-  const processors = [
-    pxtorem({
-      exclude: /node_modules/i,
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-      mediaQuery: true,
-      minPixelValue: 0,
-      replace: true,
-    }),
-    autoprefixer(),
-  ];
+	const processors = [
+		pxtorem({
+			exclude: /node_modules/i,
+			rootValue: 16,
+			unitPrecision: 5,
+			propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+			mediaQuery: true,
+			minPixelValue: 0,
+			replace: true
+		}),
+		autoprefixer()
+	];
 
-  return gulp
-    .src(paths.styles.baseScss)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
-    .pipe(bytediff.start())
-    .pipe(filter('**/*.css'))
-    .pipe(bytediff.stop((data) => formatByteMessage('autoprefixer', data)))
-    .pipe(sourcemaps.write(paths.styles.map))
-    .pipe(flatten())
-    .pipe(gulp.dest(paths.dest.buildDest)) // /public/build
-    .pipe(filter('**/*.css'))
-    .pipe(stripComments())
-    .pipe(crass())
-    .pipe(gulp.dest(paths.dest.optimized)) // /optimized/
-    .pipe(gulp.dest(paths.dest.buildDest)) // /public/build/
-    .pipe(bytediff.start())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(cssnano())
-    .pipe(gulp.dest(paths.dest.miniBuild))
-    .pipe(gulp.dest(paths.dest.compact))
-    .pipe(sizereport({ gzip: true, total: false, title: 'SIZE REPORT' }));
+	return gulp
+		.src(paths.styles.sassBase)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(processors))
+		.pipe(bytediff.start())
+		.pipe(filter('**/*.css'))
+		.pipe(bytediff.stop((data) => formatByteMessage('autoprefixer', data)))
+		.pipe(sourcemaps.write(paths.styles.map))
+		.pipe(flatten())
+		.pipe(gulp.dest(paths.dest.unoptimized)) //unoptimized
+		.pipe(filter('**/*.css'))
+		.pipe(stripComments())
+		.pipe(crass())
+		.pipe(gulp.dest(paths.dest.uncompressedOptimized)) //uncompressed-optimized/
+		.pipe(gulp.dest(paths.dest.public)) //public/
+		.pipe(bytediff.start())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cssnano())
+		.pipe(gulp.dest(paths.dest.publicMini))
+		.pipe(gulp.dest(paths.dest.compact))
+		.pipe(sizereport({ gzip: true, total: false, title: 'SIZE REPORT' }));
 }
 
 function dev() {
-  const processors = [
-    pxtorem({
-      exclude: /node_modules/i,
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-      mediaQuery: true,
-      minPixelValue: 0,
-      replace: true,
-    }),
-    autoprefixer(),
-  ];
+	const processors = [
+		pxtorem({
+			exclude: /node_modules/i,
+			rootValue: 16,
+			unitPrecision: 5,
+			propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+			mediaQuery: true,
+			minPixelValue: 0,
+			replace: true
+		}),
+		autoprefixer()
+	];
 
-  return gulp
-    .src(paths.styles.baseScss)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
-    .pipe(filter('**/*.css'))
-    .pipe(sourcemaps.write(paths.styles.map))
-    .pipe(flatten())
-    .pipe(gulp.dest(paths.dest.exportCss))
-    .pipe(filter('**/*.css'))
-    .pipe(stripComments())
-    .pipe(crass())
-    .pipe(gulp.dest(paths.dest.optimized))
-    .pipe(gulp.dest(paths.dest.buildFile))
+	return gulp
+		.src(paths.styles.sassBase)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(processors))
+		.pipe(filter('**/*.css'))
+		.pipe(sourcemaps.write(paths.styles.map))
+		.pipe(flatten())
+		.pipe(gulp.dest(paths.dest.unoptimized))
+		.pipe(filter('**/*.css'))
+		.pipe(stripComments())
+		.pipe(crass())
+		.pipe(gulp.dest(paths.dest.optimized))
+		.pipe(gulp.dest(paths.dest.public))
 
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(cssnano())
-    .pipe(gulp.dest(paths.dest.miniBuild))
-    .pipe(gulp.dest(paths.dest.compact));
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cssnano())
+		.pipe(gulp.dest(paths.dest.publicMini))
+		.pipe(gulp.dest(paths.dest.compact));
 }
 
 function test() {
-  const processors = [
-    autoprefixer(),
-    pxtorem({
-      exclude: /node_modules/i,
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-      mediaQuery: true,
-      minPixelValue: 0,
-      replace: true,
-    }),
-  ];
+	const processors = [
+		autoprefixer(),
+		pxtorem({
+			exclude: /node_modules/i,
+			rootValue: 16,
+			unitPrecision: 5,
+			propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+			mediaQuery: true,
+			minPixelValue: 0,
+			replace: true
+		})
+	];
 
-  return (
-    gulp
-      .src(paths.styles.baseScss)
-      .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(postcss(processors))
-      .pipe(bytediff.start())
-      .pipe(filter('**/*.css'))
-      .pipe(bytediff.stop((data) => formatByteMessage('autoprefixer', data)))
-      .pipe(sourcemaps.write(paths.styles.map))
-      .pipe(flatten())
-      .pipe(gulp.dest(paths.dest.buildFile))
-      .pipe(filter('**/*.css'))
-      .pipe(stripComments())
-      .pipe(crass())
-      .pipe(gulp.dest(paths.dest.optimized))
-      // .pipe(gulp.dest(paths.dest.exportCss)
-      .pipe(bytediff.start())
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(cssnano())
-      .pipe(gulp.dest(paths.dest.miniBuild))
-      .pipe(gulp.dest(paths.dest.compact))
-      .pipe(sizereport({ gzip: true, total: false, title: 'SIZE REPORT' }))
-  );
+	return (
+		gulp
+			.src(paths.styles.sassBase)
+			.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+			.pipe(postcss(processors))
+			.pipe(bytediff.start())
+			.pipe(filter('**/*.css'))
+			.pipe(bytediff.stop((data) => formatByteMessage('autoprefixer', data)))
+			.pipe(sourcemaps.write(paths.styles.map))
+			.pipe(flatten())
+			.pipe(stripComments())
+			.pipe(crass())
+			.pipe(gulp.dest(paths.dest.public))
+			.pipe(gulp.dest(paths.dest.optimized))
+			// .pipe(gulp.dest(paths.dest.exportCss)
+			.pipe(bytediff.start())
+			.pipe(rename({ suffix: '.min' }))
+			.pipe(cssnano())
+			.pipe(gulp.dest(paths.dest.publicMini))
+			.pipe(gulp.dest(paths.dest.optimized))
+			.pipe(sizereport({ gzip: true, total: false, title: 'SIZE REPORT' }))
+	);
 }
 
 function qa() {
-  const processors = [
-    autoprefixer(),
-    pxtorem({
-      exclude: /node_modules/i,
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-      mediaQuery: true,
-      minPixelValue: 0,
-      replace: true,
-    }),
-  ];
+	const processors = [
+		autoprefixer(),
+		pxtorem({
+			exclude: /node_modules/i,
+			rootValue: 16,
+			unitPrecision: 5,
+			propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+			mediaQuery: true,
+			minPixelValue: 0,
+			replace: true
+		})
+	];
 
-  return gulp
-    .src(paths.styles.baseScss)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
-    .pipe(filter('**/*.css'))
-    .pipe(sourcemaps.write(paths.styles.map))
-    .pipe(flatten())
-    .pipe(gulp.dest(paths.dest.exportCss))
-    .pipe(gulp.dest(paths.dest.buildFile))
-    .pipe(filter('**/*.css'))
-    .pipe(stripComments())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(cssnano())
-    .pipe(gulp.dest(paths.dest.miniBuild))
-    .pipe(gulp.dest(paths.dest.compact));
+	return gulp
+		.src(paths.styles.sassBase)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(processors))
+		.pipe(filter('**/*.css'))
+		.pipe(sourcemaps.write(paths.styles.map))
+		.pipe(flatten())
+		.pipe(gulp.dest(paths.dest.unoptimized))
+		.pipe(gulp.dest(paths.dest.public))
+		.pipe(filter('**/*.css'))
+		.pipe(stripComments())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cssnano())
+		.pipe(gulp.dest(paths.dest.publicMini))
+		.pipe(gulp.dest(paths.dest.compact));
+}
+
+function liveReload() {
+	//1. serve with default settings
+	var server = gls.static(); //equals to gls.static('public', 3000);
+	server.start();
+
+	//2. serve at custom port
+	// var server = gls.static('dist', 8888);
+	// server.start();
+
+	//3. serve multi folders
+	var server = gls.static(['dist', '.tmp']);
+	server.start();
+
+	//use gulp.watch to trigger server actions(notify, start or stop)
+	// gulp.watch(['static/**/*.css', 'static/**/*.html'], function (file) {
+	//   server.notify.apply(server, [file]);
 }
 
 // if (process.env.NODE_ENV === 'production') {
@@ -335,18 +357,25 @@ function qa() {
 // 	exports.build = series('sassWatch');
 // }
 const build = function () {
-  watch('./src/styles/base.scss', dev);
+	watch('public/**/*', liveReload);
 };
 // exports.sassy = sassy;
 // exports.docCss = docCss;
 exports.build = build;
-exports.sassy = sassy;
+exports.sassy = sassy; // pulls from sass/base.scss => public/base.css
+exports.compact = compact; // builds the compressed folder with source map.
 
 exports.production = production;
+/**
+ * ./sass/base.scss
+ * ./public
+ * ./
+ */
 exports.dev = dev;
 exports.test = test;
 exports.qa = qa;
 
+exports.liveReload = liveReload; // reloads pages when things in public folder change.
 exports.default = build;
 // stats is not in the series run yourself
 exports.stats = stats;
