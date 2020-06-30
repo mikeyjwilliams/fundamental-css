35c7f4f112018927570a2ec286396b01a98a64e2
Mikey Williams <manicdesigner@gmail.com>
2020-06-30 15:57:11 -0400

    change where css is save & loaded from

--- components/head.php
+++ components/head.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- <link rel="stylesheet" href="./base.css" /> -->
     <link rel="stylesheet" href="../css/build.css" />
+    
     <link rel="stylesheet"
         href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/styles/default.min.css">
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/highlight.min.js">
