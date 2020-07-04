10f4cbde9421a3815f7695ae893d6035ed41e5db
Michael Williams <manicdesigner@gmail.com>
2020-07-04 12:31:37 -0400

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,67 @@
 /** @format */
 
 // const { series, parallel, src, dest, watch } = require('gulp');
-const gulp = require('gulp');
-const autoprefixer = require('autoprefixer');
-const chalk = require('chalk');
-const cssStats = require('cssstats');
-const bytediff = require('gulp-bytediff');
-const crass = require('gulp-crass');
-const cssnano = require('gulp-cssnano');
-const csscss = require('gulp-csscss');
-const csso = require('gulp-csso');
-const flatten = require('gulp-flatten');
-const filter = require('gulp-filter');
-const rename = require('gulp-rename');
-const sizereport = require('gulp-sizereport');
-const stripComments = require('gulp-strip-css-comments');
-const stylestats = require('gulp-stylestats');
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require("gulp");
+const autoprefixer = require("autoprefixer");
+const chalk = require("chalk");
+const cssStats = require("cssstats");
+const bytediff = require("gulp-bytediff");
+const crass = require("gulp-crass");
+const cssnano = require("gulp-cssnano");
+const csscss = require("gulp-csscss");
+const csso = require("gulp-csso");
+const flatten = require("gulp-flatten");
+const filter = require("gulp-filter");
+const rename = require("gulp-rename");
+const sizereport = require("gulp-sizereport");
+const stripComments = require("gulp-strip-css-comments");
+const stylestats = require("gulp-stylestats");
+const sourcemaps = require("gulp-sourcemaps");
 
-const pixrem = require('pixrem')({
-  rootValue: 16,
-  replace: false,
-  atrules: true,
-  unitPrecision: 3,
+const pixrem = require("pixrem")({
+    rootValue: 16,
+    replace: false,
+    atrules: true,
+    unitPrecision: 3,
 });
-const pxtorem = require('postcss-pxtorem');
-const combineMediaQuery = require('postcss-combine-media-query');
+const pxtorem = require("postcss-pxtorem");
+const combineMediaQuery = require("postcss-combine-media-query");
 
-const { watch } = require('gulp');
-const postcss = require('gulp-postcss');
-const dartSass = require('gulp-dart-sass'); // will refactor in in another branch feature.
-const sass = require('gulp-sass');
+const { watch } = require("gulp");
+const postcss = require("gulp-postcss");
+const dartSass = require("gulp-dart-sass"); // will refactor in in another branch feature.
+const sass = require("gulp-sass");
 
 //** if you use dart-sass there will be breaking changes in a future release I will be converting to this. */
-sass.compiler = require('node-sass');
+sass.compiler = require("node-sass");
 
 const paths = {
-  styles: {
-    tBase: './builds/base/tail-base.css',
-    tComponent: './builds/base/tail-components.css',
-    tUtilities: './builds/base/tailwind-utilities.css',
-    sassBase: './sass/*.scss',
-    buildsBuildbase: './builds/build/*.css',
-    stylesSassbuildBase: './styles/sass-build/*.css',
+    styles: {
+        tBase: "./builds/base/tail-base.css",
+        tComponent: "./builds/base/tail-components.css",
+        tUtilities: "./builds/base/tailwind-utilities.css",
+        sassBase: "./sass/*.scss",
+        buildsBuildbase: "./builds/build/*.css",
+        stylesSassbuildBase: "./styles/sass-build/*.css",
 
-    cssBuildBuild: './css/build/build.css',
-    map: './',
-  },
-  dest: {
-    buildMap: './',
-    css: './css/',
-    cssBase: './css/*.css',
-    cssMini: './css/mini/',
-    miniMap: './',
-    uncompressedCss: './uncompressed-css/',
-    compressedCss: './compressed-css/',
-    unoptimized: './unoptimized/',
-    optimized: './optimized/',
-    uncompressedOptimized: './uncompressed-optimized/',
-    compressedOptimized: './compressed-optimized/',
+        cssBuildBuild: "./css/build/build.css",
+        map: "./",
+    },
+    dest: {
+        buildMap: "./",
+        css: "./css/",
+        cssBase: "./css/*.css",
+        cssMini: "./css/mini/",
+        miniMap: "./",
+        uncompressedCss: "./uncompressed-css/",
+        compressedCss: "./compressed-css/",
+        unoptimized: "./unoptimized/",
+        optimized: "./optimized/",
+        uncompressedOptimized: "./uncompressed-optimized/",
+        compressedOptimized: "./compressed-optimized/",
 
-    stats: './stats/',
-  },
+        stats: "./stats/",
+    },
 };
 
 /**
@@ -69,37 +69,37 @@ const paths = {
  * @description reports back a file size of the css data.
  */
 function humanFileSize(size) {
-  let i = Math.floor(Math.log(size) / Math.log(1024));
-  return (
-    (size / Math.pow(1024, i)).toFixed(2) * 1 +
-    '' +
-    ['B', 'kB', 'MB', 'GB', 'TB'][i]
-  );
+    let i = Math.floor(Math.log(size) / Math.log(1024));
+    return (
+        (size / Math.pow(1024, i)).toFixed(2) * 1 +
+        "" +
+        ["B", "kB", "MB", "GB", "TB"][i]
+    );
 }
 
 function formatByteMessage(source, data) {
-  const prettyStartSize = humanFileSize(data.startSize);
-  let message = '';
-  console.log('D ', data);
-  if (data.startSize !== data.endSize) {
-    const change = data.savings > 0 ? 'saved' : 'gained';
-    const prettySavings = humanFileSize(Math.abs(data.savings));
-    let prettyEndSize = humanFileSize(data.endSize);
+    const prettyStartSize = humanFileSize(data.startSize);
+    let message = "";
+    console.log("D ", data);
+    if (data.startSize !== data.endSize) {
+        const change = data.savings > 0 ? "saved" : "gained";
+        const prettySavings = humanFileSize(Math.abs(data.savings));
+        let prettyEndSize = humanFileSize(data.endSize);
 
-    if (data.endSize > data.startSize) {
-      prettyEndSize = chalk.yellow(prettyEndSize);
-    }
-    if (data.endSize < data.startSize) {
-      prettyEndSize = chalk.green(prettyEndSize);
-    }
+        if (data.endSize > data.startSize) {
+            prettyEndSize = chalk.yellow(prettyEndSize);
+        }
+        if (data.endSize < data.startSize) {
+            prettyEndSize = chalk.green(prettyEndSize);
+        }
 
-    message = chalk`${change} ${prettySavings} (${prettyStartSize} -> {bold ${prettyEndSize}})`;
-  } else {
-    message = chalk`kept original filesize. ({bold ${prettyStartSize}})`;
-  }
-  return chalk`{cyan ${source.padStart(12, ' ')}}: {bold ${
-    data.fileName
-  }} ${message}`;
+        message = chalk`${change} ${prettySavings} (${prettyStartSize} -> {bold ${prettyEndSize}})`;
+    } else {
+        message = chalk`kept original filesize. ({bold ${prettyStartSize}})`;
+    }
+    return chalk`{cyan ${source.padStart(12, " ")}}: {bold ${
+        data.fileName
+    }} ${message}`;
 }
 
 /**
@@ -107,14 +107,14 @@ function formatByteMessage(source, data) {
  * @description converts sass files to css
  * @implements gulp-sass
  */
-function sassy() {
-  return gulp
-    .src(paths.styles.sassBase)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write(paths.dest.buildMap))
-    .pipe(gulp.dest(paths.dest.css));
-}
+gulp.task("sassy", () => {
+    return gulp
+        .src(paths.styles.sassBase)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(sourcemaps.write(paths.dest.buildMap))
+        .pipe(gulp.dest(paths.dest.css));
+});
 
 /**
  * @name stats
@@ -122,15 +122,15 @@ function sassy() {
  * @implements gulp-stylestats
  */
 function stats() {
-  return gulp
-    .src(paths.dest.cssBase)
-    .pipe(
-      stylestats({
-        type: 'json',
-        outfile: true,
-      })
-    )
-    .pipe(gulp.dest(paths.dest.stats));
+    return gulp
+        .src(paths.dest.cssBase)
+        .pipe(
+            stylestats({
+                type: "json",
+                outfile: true,
+            })
+        )
+        .pipe(gulp.dest(paths.dest.stats));
 }
 
 /**
@@ -138,26 +138,26 @@ function stats() {
  * @description puts fallbacks for specific rem cases in px
  * @implements gulp-postcss, gulp-autoprefixer, postcss-pxtorem
  */
-function pixeltorem() {
-  const processors = [
-    pxtorem({
-      exclude: /node_modules/i,
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-      mediaQuery: true,
-      minPixelValue: 0,
-      replace: true,
-    }),
-    autoprefixer(),
-  ];
+gulp.task("pixeltorem", () => {
+    const processors = [
+        pxtorem({
+            exclude: /node_modules/i,
+            rootValue: 16,
+            unitPrecision: 5,
+            propList: ["font", "font-size", "line-height", "letter-spacing"],
+            mediaQuery: true,
+            minPixelValue: 0,
+            replace: true,
+        }),
+        autoprefixer(),
+    ];
 
-  return gulp
-    .src(paths.styles.stylesSassbuildBase)
-    .pipe(postcss(processors))
-    .pipe(gulp.dest(paths.dest.css))
-    .pipe(gulp.dest(paths.dest.uncompressedCss));
-}
+    return gulp
+        .src(paths.styles.stylesSassbuildBase)
+        .pipe(postcss(processors))
+        .pipe(gulp.dest(paths.dest.css))
+        .pipe(gulp.dest(paths.dest.uncompressedCss));
+});
 
 /**
  * @name renameMini
@@ -180,18 +180,19 @@ function pixeltorem() {
  * @description minify css styles
  * @implements gulp-sourcemaps, gulp-cssnano
  */
-function compact() {
-  return gulp
-    .src(paths.styles.buildsBuildbase)
-    .pipe(sourcemaps.init())
-    .pipe(cssnano())
-    .pipe(sourcemaps.write(paths.dest.miniMap))
-    .pipe(gulp.dest(paths.dest.compressedCss));
-}
 
-function check() {
-  return gulp.src(paths.dest.css).pipe(csscss());
-}
+gulp.task("compact", () => {
+    return gulp
+        .src(paths.styles.buildsBuildbase)
+        .pipe(sourcemaps.init())
+        .pipe(cssnano())
+        .pipe(sourcemaps.write(paths.dest.miniMap))
+        .pipe(gulp.dest(paths.dest.compressedCss));
+});
+
+gulp.task("check", () => {
+    return gulp.src(paths.dest.css).pipe(csscss());
+});
 // not working properly
 // function production() {
 //   const processors = [
@@ -316,39 +317,40 @@ function check() {
 //     // .pipe(sizereport({ gzip: true, total: true, title: 'SIZE REPORT' }))
 //   );
 // }
-function qa() {
-  const processors = [
-    pxtorem({
-      exclude: /node_modules/i,
-      rootValue: 16,
-      unitPrecision: 5,
-      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-      mediaQuery: true,
-      minPixelValue: 0,
-      replace: true,
-    }),
-    autoprefixer(),
-    combineMediaQuery(),
-  ];
+gulp.task("qa", () => {
+    const processors = [
+        pxtorem({
+            exclude: /node_modules/i,
+            rootValue: 16,
+            unitPrecision: 5,
+            propList: ["font", "font-size", "line-height", "letter-spacing"],
+            mediaQuery: true,
+            minPixelValue: 0,
+            replace: true,
+        }),
+        autoprefixer(),
+        combineMediaQuery(),
+    ];
 
-  return gulp
-    .src(paths.styles.sassBase)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
-    .pipe(filter('**/*.css'))
-    .pipe(sourcemaps.write(paths.styles.map))
-    .pipe(flatten())
-    .pipe(gulp.dest(paths.dest.unoptimized))
-    .pipe(stripComments())
-    .pipe(gulp.dest(paths.dest.css))
-    .pipe(filter('**/*.css'))
-    .pipe(stripComments())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(cssnano())
-    .pipe(gulp.dest(paths.dest.cssMini))
-    .pipe(gulp.dest(paths.dest.compressedOptimized));
-}
+    return gulp
+        .src(paths.styles.sassBase)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(postcss(processors))
+        .pipe(filter("**/*.css"))
+        .pipe(sourcemaps.write(paths.styles.map))
+        .pipe(flatten())
+        .pipe(gulp.dest(paths.dest.unoptimized))
+        .pipe(stripComments())
+        .pipe(gulp.dest(paths.dest.css))
+        .pipe(filter("**/*.css"))
+        .pipe(stripComments())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(cssnano())
+        .pipe(gulp.dest(paths.dest.cssMini))
+        .pipe(gulp.dest(paths.dest.compressedOptimized));
+});
+
 exports.compact = compact; // builds the compressed folder with source map.
 
 // exports.production = production;
