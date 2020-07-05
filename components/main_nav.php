251208efdd853f0e1311cc8ef6bf355f693a14ef
Michael Williams <manicdesigner@gmail.com>
2020-07-04 22:42:24 -0400

    working on nav system

--- components/main_nav.php
+++ components/main_nav.php
@@ -4,7 +4,41 @@
             <li>
                 <p class='text--3xl'>Documentation</p>
             </li>
-
+            <li class='pt--2'>
+                <a href="#">Installation</a>
+            </li>
+            <li class='pt--2'>
+                <a href="#">Release notes</a>
+            </li>
+            <li class='pt--2'>
+                <a href="#">Controlling file size</a>
+            </li>
+            <li class='pt--2'>
+                <a href="#">Browser support</a>
+            </li>
+            <li class='pt--2'>
+                <hr />
+            </li>
+            <li>
+                <p class='text--3xl'>
+                    core concepts
+                </p>
+            </li>
+            <li class='pt--2'>
+                <a href='#'>Utility first</a>
+            </li>
+            <li class='pt--2'>
+                <a href='#'>Responsive Design</a>
+            </li>
+            <li class='pt--2'>
+                <a href='#'>Pseudo-Class Variants</a>
+            </li>
+            <li class='pt--2'>
+                <a href='#'>Adding Base Styles</a>
+            </li>
+            <li class='mt--2'>
+                <a href='#'>Extracting Components</a>
+            </li>
         </ul>
     </nav>
 </div>
