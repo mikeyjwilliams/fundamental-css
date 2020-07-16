25b23f16fa8610ad8b93437f393210e3e9866144
Michael Williams <manicdesigner@gmail.com>
2020-07-16 11:45:57 -0400

    add more plugins to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,18 +14,61 @@ module.exports = function (grunt) {
             dist: {
                 files: {
                     "./unoptimized/build.css": "./sass/build.scss",
-                    "./public/css/build.css": "./sass/build.scss",
                 },
             },
         },
         stripCssComments: {
             dist: {
                 files: {
-                    "./public/css/build.css": "./public/css/build.css",
+                    "./uncompressed-optimized/build.css":
+                        "./unoptimized/build.css",
                 },
+            },
+        },
+        cmq: {
+            options: {
+                log: false,
+            },
+            css: {
+                files: {
+                    "./compressed-optimized": [
+                        "./uncompressed-optimized/*.css",
+                    ],
+                    "./public/css/build.css": [
+                        "./uncompressed-optimized/*.css",
+                    ],
+                },
+            },
+        },
+        postcss: {
+            options: {
+                // map: true, // inline sourcemaps
+
+                // or
+                map: {
+                    inline: false, // save all sourcemaps as separate files...
+                    annotation: "public/css/maps/", // ...to the specified directory
+                },
+
+                processors: [
+                    require("pixrem")(), // add fallbacks for rem units
+                    require("autoprefixer")({ browsers: "last 2 versions" }), // add vendor prefixes
+                    // require("cssnano")(), // minify the result
+                ],
+            },
+            dist: {
+                src: "./public/css/*.css",
             },
         },
     });
 
-    grunt.registerTask("default", ["sass", "stripCssComments"]);
+    grunt.loadNpmTasks("grunt-combine-media-queries");
+    grunt.loadNpmTasks("grunt-postcss");
+
+    grunt.registerTask("default", [
+        "sass",
+        "stripCssComments",
+        "cmq",
+        "postcss",
+    ]);
 };
